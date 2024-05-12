36225d9d42841c80e43d0354dc485c14322b443e
Zuhriddin1 <anarimon1985@gmail.com>
2024-05-12 22:27:22 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+const App: FC = () => {
+  return (
+    <div>
+      App
+      {/* Video yoq edi va man homeworkni qilla olmadim dushanba kunigacha qilib qoyaman  */}
+    </div>
+  );
+};
+export default App;
