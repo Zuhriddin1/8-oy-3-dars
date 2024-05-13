2144c721c7d4eca95258b297f222ad18f84bb547
Zuhriddin1 <anarimon1985@gmail.com>
2024-05-13 19:33:38 +0300

    .

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -21,7 +21,6 @@ const TodoItem = () => {
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
           <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
               ✕
             </button>
